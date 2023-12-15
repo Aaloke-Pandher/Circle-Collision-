e54ee022aa17bb9d3fbaf813f64a64755e01649a
Aaloke-Pandher <98190131+Aaloke-Pandher@users.noreply.github.com>
2023-12-15 12:56:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ class Player:
         self.xspeed = 5 
         self.yspeed = 5  
     def drawPlayer(self, screen): 
-        pygame.draw.ellipse(screen, Blue, [self.x, self.y, self.w, self.h], 2) 
+        pygame.draw.ellipse(screen, Blue, [self.x, self.y, self.w, self.h]) 
+        
 
 class Food: 
     def __init__(self, x, y, w, h, c): 
@@ -30,7 +31,7 @@ class Food:
         self.h = h   
         self.c = c
     def drawFood(self, screen):   
-        pygame.draw.ellipse(screen, self.c, [self.x, self.y, self.w, self.h]) 
+        pygame.draw.ellipse(screen, self.c, [self.x, self.y, self.w, self.h], 2) 
 # List of Food 
 food = []   
 
@@ -46,16 +47,18 @@ def follow_object(ob2, speed):
     run = (mouse_position()[0] - ob2.x) 
     rise = (mouse_position()[1] - ob2.y) 
     d = math.sqrt(rise**2 + run**2)
-    dx = (speed * rise) / d 
-    dy = (speed * run) / d 
-    return (dx, dy) 
+    dy = (speed * rise) / d 
+    dx = (speed * run) / d 
+    ob2.x += dx 
+    ob2.y += dy
+    return dx, dy 
 
 
 
 # Collision Function 
 def rectCollision(rect1, rect2): 
-   return rect1.x < rect2.x + rect2.w and rect1.y < rect2.y + rect2.h and rect1.x + rect1.w > rect2.x and rect1.y + rect1.h > rect2.y 
-
+   if rect1.x < rect2.x + rect2.w and rect1.y < rect2.y + rect2.h and rect1.x + rect1.w > rect2.x and rect1.y + rect1.h > rect2.y:
+        return 1
 player = Player(400, 300, 20, 20)
 # Main function 
 def main():
@@ -79,8 +82,9 @@ def main():
     
         # Draw 
         screen.fill(White) 
-        player.drawPlayer(screen)   
-        if frameCount % 6000 == 0: 
+        player.drawPlayer(screen)  
+        follow_object(player, 2)  
+        if frameCount % 60 == 0: 
             randColor = [random.randrange(0, 255), random.randrange(0, 255), random.randrange(0, 255)]
             wHRan = random.randrange(10, 15)
             food.append(Food(random.randrange(0, 800), random.randrange(0, 600), wHRan, wHRan, randColor)) 
@@ -90,6 +94,7 @@ def main():
                 player.w = 1/8 * food[i].w + player.w
                 player.h = 1/8 * food[i].h + player.h 
                 food.pop(i)
+                break 
         pygame.display.flip()
  
         # --- Limit frames
